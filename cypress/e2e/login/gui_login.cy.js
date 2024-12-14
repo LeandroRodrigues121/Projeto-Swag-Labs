d9b8146d32082eb5b28f19ca043a6026023b0eee
Leandro <leandro121paiva121@gmail.com>
2024-12-14 00:15:06 -0300

    Criação do caso de teste Verificar a lista de produto e do Validar login Incorreto

--- cypress/e2e/login/gui_login.cy.js
+++ cypress/e2e/login/gui_login.cy.js
@@ -9,5 +9,18 @@ describe('Testes Funcional de Login', () => {
         cy.get('[data-test="title"]').should('contain','Products')
     });
 
+    it('Verificar a lista de produto', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('[data-test="login-button"]').click()
+        cy.get(':nth-child(1) > [data-test="inventory-item-description"]').should('contain','Sauce Labs Backpack')
+        cy.get(':nth-child(2) > [data-test="inventory-item-description"]').should('contain','Sauce Labs Bike Light')
+        cy.get(':nth-child(3) > [data-test="inventory-item-description"]').should('contain','Sauce Labs Bolt T-Shirt')
+        cy.get(':nth-child(4) > [data-test="inventory-item-description"]').should('contain','Sauce Labs Fleece Jacket')
+        cy.get(':nth-child(5) > [data-test="inventory-item-description"]').should('contain','Sauce Labs Onesie')
+        cy.get(':nth-child(6) > [data-test="inventory-item-description"]').should('contain','Test.allTheThings() T-Shirt (Red)')
+    });
+
 
 });

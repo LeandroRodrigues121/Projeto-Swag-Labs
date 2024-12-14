b58c725aed95240f17a58fb8e91088f1f77760a7
Leandro <leandro121paiva121@gmail.com>
2024-12-14 00:22:13 -0300

    Criação do caso de teste 'Adicionar ao Carrinho' e o 'Adicionar ao carrinho e volta para a tela pricipal'

--- cypress/e2e/login/gui_login.cy.js
+++ cypress/e2e/login/gui_login.cy.js
@@ -39,5 +39,29 @@ describe('Testes Funcional de Login', () => {
         cy.get(':nth-child(6) > [data-test="inventory-item-description"]').should('contain','Test.allTheThings() T-Shirt (Red)')
     });
 
+    it('Adicionar ao Carrinho', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+        cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+        cy.get('[data-test="shopping-cart-link"]').click()
+    });
+
+
+    it('Adicionar ao Carrinho e volta para a tela pricipal', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+        cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+        cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+        cy.get('[data-test="shopping-cart-link"]').click()
+        cy.get('[data-test="continue-shopping"]').click()
+        cy.get('[data-test="title"]').should('contain','Products')
+    });
 
 });

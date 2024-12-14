4c1c855f03bc7dd4d4e6eaa557be657c7ca0f935
Leandro <leandro121paiva121@gmail.com>
2024-12-14 00:19:48 -0300

    Criação do caso de teste Validar Login Incorreto e validar Senha Incorreta

--- cypress/e2e/login/gui_login.cy.js
+++ cypress/e2e/login/gui_login.cy.js
@@ -9,6 +9,23 @@ describe('Testes Funcional de Login', () => {
         cy.get('[data-test="title"]').should('contain','Products')
     });
 
+
+    it('Validando Login Incorreta ', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get('[data-test="username"]').type("standard_user2323")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]').should('contain','Epic sadface: Username and password do not match any user in this service')
+    });
+
+    it('Validando Senha Incorreta ', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="password"]').type("secret_sauce1223")
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]').should('contain','Epic sadface: Username and password do not match any user in this service')
+    });
+
     it('Verificar a lista de produto', () => {
         cy.visit("https://www.saucedemo.com/")
         cy.get('[data-test="username"]').type("standard_user")
